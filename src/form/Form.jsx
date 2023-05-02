ae6db1a668509b1d27653ff57686be2e59682b5e
shubham <pghotkar931@gmail.com>
2023-05-03 01:10:48 +0530

    second commit

---
+++ src/form/Form.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PersonalDetails from "./PersonalDetails";
+import "./style.css";
+export default function Form() {
+  return (
+    <>
+      <div className="container">
+        <Form>
+          <PersonalDetails />
+        </Form>
+      </div>
+    </>
+  );
+}
---
+++ src/form/OtherDetails.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function OtherDetails() {
+  return (
+    <>
+      <div className="container"></div>
+    </>
+  );
+}
